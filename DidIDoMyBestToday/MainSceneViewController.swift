f7d69b18a78bba374820d37d735cbe3feac9c0e2
wolframhwang <hzw94@naver.com>
2022-06-12 11:17:00 +0900

    initial Commit: MainScene ViewController

--- DidIDoMyBestToday/MainSceneViewController.swift
+++ DidIDoMyBestToday/MainSceneViewController.swift
@@ -7,11 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainSceneViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
