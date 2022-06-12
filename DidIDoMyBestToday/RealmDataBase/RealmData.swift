e54338a567db47d4b894180975da1813307c70fa
wolframhwang <hzw94@naver.com>
2022-06-12 15:32:46 +0900

    [feat] RealmManager: Realm을 사용하기 위한 Object + Manager 추가사항 반영

---
+++ DidIDoMyBestToday/RealmDataBase/RealmData.swift
@@ -0,0 +1,16 @@
+//
+//  RealmData.swift
+//  DidIDoMyBestToday
+//
+//  Created by 황지웅 on 2022/06/12.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmData: Object {
+    @objc dynamic var title = ""
+    @objc dynamic var contents = ""
+    @objc dynamic var satisfy: Int = Int()
+    @objc dynamic var date: Date = Date()
+}
---
+++ DidIDoMyBestToday/RealmDataBase/RealmDataManager.swift
@@ -0,0 +1,13 @@
+//
+//  RealmDataManager.swift
+//  DidIDoMyBestToday
+//
+//  Created by 황지웅 on 2022/06/12.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmDataManager {
+    
+}
