b6b268213ee71db3a42523b7098e6399dc26af13
wolframhwang <hzw94@naver.com>
2022-06-12 16:11:13 +0900

    [Test: refactor] Realm: Realm에 관한 수정사항반영

--- DidIDoMyBestTodayTests/RealmTest/RealmTest.swift
+++ DidIDoMyBestTodayTests/RealmTest/RealmTest.swift
@@ -29,22 +29,22 @@ class RealmTest: XCTestCase {
         data1.title = "Data1"
         data1.contents = "CData1"
         data1.satisfy = 3
-        data1.date = Date()
+        data1.date = "2022-06-12"
         
         data2.title = "Data2"
         data2.contents = "CData2"
         data2.satisfy = 4
-        data2.date = Date()
+        data2.date = "2022-06-12"
         
         data3.title = "Data3"
         data3.contents = "CData3"
         data3.satisfy = 5
-        data3.date = Date()
+        data3.date = "2022-06-12"
         
         data4.title = "Data4"
         data4.contents = "CData4"
         data4.satisfy = 1
-        data4.date = Date()
+        data4.date = "2022-06-12"
         
 
         let data = realm.getRealmDataInfo(condition: nil)
