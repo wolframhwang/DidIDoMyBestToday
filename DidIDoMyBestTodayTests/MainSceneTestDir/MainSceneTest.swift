a78a4f8e6e82fb54cbc34258162027f6cae2fea5
wolframhwang <hzw94@naver.com>
2022-06-12 17:24:33 +0900

    [Test: Red] MainScene: Task Data 받아오는지 테스트하기 위한 테스트 코드

--- DidIDoMyBestTodayTests/MainSceneTestDir/MainSceneTest.swift
+++ DidIDoMyBestTodayTests/MainSceneTestDir/MainSceneTest.swift
@@ -41,11 +41,18 @@ class MainSceneTest: XCTestCase {
         XCTAssertEqual(sut.getToday(), "2022-06-12")
     }
     
+    func test_toDays() {
+        sut.todaysTask()
+        print(sut.getTasks())
+        XCTAssertEqual(sut.getTasks().count, 4)
+    }
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
+    
 
 }
