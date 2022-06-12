5590ec93472701a306d0416a352e9155ac82c462
wolframhwang <hzw94@naver.com>
2022-06-12 19:35:13 +0900

    [Test: 생성] WriteScene: WriteScene Test를 위한 테스팅 + Mock 코드 생성

---
+++ DidIDoMyBestTodayTests/MainSceneTestDir/WriteTodoSceneTestDir/MockWriteSceneViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MockWriteSceneViewController.swift
+//  DidIDoMyBestTodayTests
+//
+//  Created by 황지웅 on 2022/06/12.
+//
+
+import XCTest
+@testable import DidIDoMyBestToday
+
+class MockWriteSceneViewController: WriteTodoTaskSceneProtocol {
+
+    
+}
