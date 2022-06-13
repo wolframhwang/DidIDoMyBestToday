4484e3c352baf92bc64b5d136cb952df2c549576
wolframhwang <hzw94@naver.com>
2022-06-13 19:03:28 +0900

    [feat] WriteScene: WriteScene에서 Title에 해당하는 Cell의 Presenter 추가 반영

---
+++ DidIDoMyBestToday/MainScene/WriteTodoTaskScene/WrieTitleCell/WriteTitlePresenter.swift
@@ -0,0 +1,27 @@
+//
+//  WriteTitlePresenter.swift
+//  DidIDoMyBestToday
+//
+//  Created by 황지웅 on 2022/06/12.
+//
+
+import Foundation
+import UIKit
+
+class WriteTitlePresenter: NSObject {
+    private weak var writeTitleCell: WriteProtocol?
+    private var text: String = ""
+    init(writeTitelCell: WriteProtocol?) {
+        self.writeTitleCell = writeTitelCell
+    }
+    func setView(writeTitelCell: WriteProtocol?) {
+        self.writeTitleCell = writeTitelCell
+    }
+    func didChangeTextField(text: String) {
+        self.text = text
+    }
+    
+    func getText() -> String {
+        return text
+    }
+}
