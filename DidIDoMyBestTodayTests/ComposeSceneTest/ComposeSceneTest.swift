629c1a60d60584c6ebc486549eac90698e215119
wolframhwang <hzw94@naver.com>
2022-06-13 20:03:24 +0900

    [Test] ComposeScene: ComposeScene 테스트 하기 위한 테스트 베이스 작성

---
+++ DidIDoMyBestTodayTests/ComposeSceneTest/ComposeSceneTest.swift
@@ -0,0 +1,23 @@
+//
+//  ComposeSceneTest.swift
+//  DidIDoMyBestTodayTests
+//
+//  Created by 황지웅 on 2022/06/13.
+//
+
+import XCTest
+@testable import DidIDoMyBestToday
+
+class ComposeSceneTest: XCTestCase {
+    var sut: ComposeScenePresenter!
+    var viewController: MockComposeSceneViewController!
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    override func tearDown() {
+        
+        super.tearDown()
+    }
+}
---
+++ DidIDoMyBestTodayTests/ComposeSceneTest/MockComposeSceneViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MockComposeSceneViewController.swift
+//  DidIDoMyBestTodayTests
+//
+//  Created by 황지웅 on 2022/06/13.
+//
+
+import Foundation
+@testable import DidIDoMyBestToday
+
+class MockComposeSceneViewController: ComposeSceneProtocol {
+    
+}
